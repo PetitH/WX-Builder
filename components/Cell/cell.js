7c539d51c5b6080831446991dc3891fc36ef358b
张健鸿 <Petit_h@126.com>
2019-04-03 09:01:41 +0800

    自定义组件通信

--- components/Cell/cell.js
+++ components/Cell/cell.js
@@ -1,6 +1,16 @@
 // components/Cell/cell.js
 Component({
   "properties": {
-    
+    title: {
+      type: String,
+      value: 'cell模板'
+    }
+  },
+  data: {
+  },
+  "methods": {
+    _linkFun() {
+      this.triggerEvent("linkFun")
+    }
   }
 })
